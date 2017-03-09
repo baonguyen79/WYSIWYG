69336bfc2735a0f68bfb42156a7443dd3d5daa25
nguyen,bao <bao.nguyen@ingramcontent.com>
2017-03-09 17:32:26 -0600

    add more person

--- wysiwyg.js
+++ wysiwyg.js
@@ -1,3 +1,6 @@
+//******************************************
+// create Famous persons
+//******************************************
 var FamousPersons = [
 
 	{
@@ -9,16 +12,26 @@ var FamousPersons = [
 	    birth: 1747,
 	    death: 1797}
 	  },
+
 	  {
-	  title: "Samurai2",
-	  name: "Tomoe Gozen2",
-	  bio: " 2 Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
-	  image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
+	  title: "Mahatma Gandhi",
+	  name: "Mohandas Karamchand Gandhi",
+	  bio: "Born and raised in a Hindu merchant caste family in coastal Gujarat, western India, and trained in law at the Inner Temple, London, Gandhi first employed nonviolent civil disobedience as an expatriate lawyer in South Africa, in the resident Indian community's struggle for civil rights. After his return to India in 1915,",
+	  image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Portrait_Gandhi.jpg/800px-Portrait_Gandhi.jpg",
 	  lifespan: {
-	    birth: 1747,
-	    death: 1797
+	    birth: 1869,
+	    death: 1948}
+	  },
+
+	  {
+	  title: "Albert Einstein",
+	  name: "Albert Einstein",
+	  bio: "German-born theoretical physicist. He developed the theory of relativity, one of the two pillars of modern physics (alongside quantum mechanics).[1][5]:274 Einstein's work is also known for its influence on the philosophy of science.[6][7] Einstein is best known in popular culture for his mass–energy equivalence formula E = mc2 (which has been dubbed the world most famous equation.",
+	  image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Einstein_1921_by_F_Schmutzer_-_restoration.jpg/220px-Einstein_1921_by_F_Schmutzer_-_restoration.jpg",
+	  lifespan: {
+	    birth: 1879,
+	    death: 1955}
 	  }
-	}
 
 ];
 
@@ -30,13 +43,13 @@ var personSection = "";
 var personImg = "";
 var personFooter = "";
 
+// Create person DOM elements
 for (var i = 0; i<FamousPersons.length; i++) {
-  // Give each person element a unique identifier
 
   personDiv = document.createElement("div");
   personDiv.className = "person_container";
   personDiv.id = `"person-${i}"`;
-  // personDiv.innerHTML = `class="person__container" id="person--${i}"`;
+
   personHeader = document.createElement("header");
   personHeader.innerHTML = `${FamousPersons[i].name} , ${FamousPersons[i].title}`;
 
@@ -57,7 +70,6 @@ for (var i = 0; i<FamousPersons.length; i++) {
   outputEl.appendChild(personDiv);
 }
 
-
 // Now containerEl will have elements in it
 var containerEl = document.getElementsByClassName("person_container");
 
@@ -65,11 +77,12 @@ var containerEl = document.getElementsByClassName("person_container");
 for (var i = 0; i < containerEl.length; i++) {
   containerEl[i].addEventListener("click", personEvent);
     
-  };
+};
 
-
+//******************************************
+// Event Handler function when person element click
+//******************************************
 function personEvent(e){
-// Logic to execute when the element is clicked
  
     e.currentTarget.classList.add("dashed");
     this.childNodes[2].id = "select";
@@ -81,6 +94,9 @@ function personEvent(e){
 
 } 
 
+//******************************************
+// Event Handler function when text area 
+//******************************************
 function textBoxEvent (e){
 	e.preventDefault();
 	var select = document.getElementById("select");
