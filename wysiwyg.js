499e87e6dcb2e93c5db543cea4d02138edea3486
nguyen,bao <bao.nguyen@ingramcontent.com>
2017-03-09 00:25:54 -0600

    commit 2

---
+++ wysiwyg.js
@@ -0,0 +1,100 @@
+var FamousPersons = [
+
+	{
+	  title: "Samurai",
+	  name: "Tomoe Gozen",
+	  bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
+	  image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
+	  lifespan: {
+	    birth: 1747,
+	    death: 1797}
+	  },
+	  {
+	  title: "Samurai2",
+	  name: "Tomoe Gozen2",
+	  bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
+	  image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
+	  lifespan: {
+	    birth: 1747,
+	    death: 1797
+	  }
+	}
+
+];
+
+
+var outputEl = document.getElementById("outputEl");
+var personDiv = "";
+var personHeader = "";
+var personSection = "";
+var personImg = "";
+var personFooter = "";
+
+for (var i = 0; i<FamousPersons.length; i++) {
+  // Give each person element a unique identifier
+
+  personDiv = document.createElement("div");
+  personDiv.className = "person_container";
+  personDiv.id = `"person-${i}"`;
+  // personDiv.innerHTML = `class="person__container" id="person--${i}"`;
+  personHeader = document.createElement("header");
+  personHeader.innerHTML = `${FamousPersons[i].name} , ${FamousPersons[i].title}`;
+
+  personImg = document.createElement("IMG");
+  personImg.setAttribute("src", `${FamousPersons[i].image}`);
+
+  personSection = document.createElement('section');
+  personSection.innerHTML = `${FamousPersons[i].bio}`;
+
+
+  personFooter = document.createElement('footer');
+  personFooter.innerHTML = `${FamousPersons[i].lifespan.birth} - ${FamousPersons[i].lifespan.death}`;
+
+  personDiv.appendChild(personHeader);
+  personDiv.appendChild(personImg);  
+  personDiv.appendChild(personSection);
+  personDiv.appendChild(personFooter);
+  outputEl.appendChild(personDiv);
+}
+
+
+// Now containerEl will have elements in it
+var containerEl = document.getElementsByClassName("person_container");
+
+// Event listeners are created
+for (var i = 0; i < containerEl.length; i++) {
+  containerEl[i].addEventListener("click", personEvent);
+    
+  };
+
+
+function personEvent(e){
+// Logic to execute when the element is clicked
+    // e.target.style.border = "dashed";
+    e.target.classList.add("dashed");
+    this.childNodes[2].id = "select";
+    var textBox = document.getElementById("inputText");
+    textBox.innerHTML = this.childNodes[2].textContent;
+    textBox.focus();
+    textBox.addEventListener("keyup", textBoxEvent);
+
+} 
+
+function textBoxEvent (e){
+	var select = document.getElementById("select");
+
+
+	if (e.keyCode === 13){
+			this.value = "";
+
+			var a = document.getElementById("select");
+			a.parentNode.classList.remove("dashed");
+			a.id = "";
+
+			console.log(e , ' ' , a );
+   
+		}
+	else {
+			select.innerText = this.value;
+	}
+}
